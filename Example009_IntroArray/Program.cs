a7bfcef8234aeaac38f190638b96ff23510b0fbd
IgorZhil <103311402+IgorZhil@users.noreply.github.com>
2022-08-09 21:14:26 +0500

    Example 009 IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,11 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int resalt = arg1;
+    if (arg2 > resalt) resalt = arg2;
+    if (arg3 > resalt) resalt = arg3;
+    return resalt;
+}//              0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+array[0] = 12;
+Console.WriteLine(array[5]);
+
